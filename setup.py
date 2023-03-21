214e57b315e3882c6ef41f6ad06e4b47be28e3ca
Ahmed Magaji <magajiahmed98@gmail.com>
2023-03-21 03:09:12 +0100

    setup and requirement

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open('requirements.txt') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [r.replace('\n', ' ') for r in requirements]
+    
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Ahmed',
+    author_email='magajiahmed98@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
