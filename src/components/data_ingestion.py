73c4d89fce2df4cd55f77396ab4f31ec25d497cf
Ahmed Magaji <magajiahmed98@gmail.com>
2023-03-29 10:50:08 +0100

    modified data ingestion main

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
 
 # use a dataclass decorator to create a class specifically for storing data 
 # there is no need to define a constructor using init function
@@ -51,4 +53,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transform(train_data, test_data)
