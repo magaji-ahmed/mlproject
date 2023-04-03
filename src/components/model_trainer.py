6720177ad415b75c28d3619b1cd21e44ad5e44e1
Ahmed Magaji <magajiahmed98@gmail.com>
2023-04-03 12:08:00 +0100

    model trainer update

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -21,7 +21,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_arr, test_arr, preprocessor_path):
+    def initiate_model_trainer(self, train_arr, test_arr, preprocessor_path=None):
         try:
 
             # separate the target variable in the train and test arrays
@@ -56,7 +56,7 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]
 
-            best_model = model_report[best_model_name]
+            best_model = models[best_model_name]
 
             if best_model_score < 0.6:
                 raise CustomException('No best Model Found!!!')
@@ -65,9 +65,9 @@ class ModelTrainer:
             save_object(file_path=self.model_trainer_config.trained_model_file_path,
                             obj=best_model)
 
-            predicted = best_model.predict(X_test)
 
-            return r2_score(y_test, predicted)
+
+            return (best_model, best_model_name, best_model_score)
             
 
         except Exception as e:
